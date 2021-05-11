14a76ffc37ebc69d4f11a62c2e5127fbf8ec03c7
harshithva <harshith11032001@gmail.com>
2021-05-11 11:35:10 +0530

    new design

--- popup.js
+++ popup.js
@@ -2,34 +2,38 @@
 
 let t = null;
 let startButton = document.getElementById('startButton');
-let countValue = document.getElementById('countValue').value;
+let count = document.getElementById('count').value;
 
-function autoLeaveGoogleMeet() {
-    alert("checking members.....");
-    //check for active members in meet
-    if (document.querySelector(".gV3Svc>span").nextElementSibling.innerText < countValue) {
-        try {
-            window.document.querySelector(".FbBiwc").click(); //click the endcall btn on meet
-            alert('Ooo...Meet has been ended');
-        } catch {
-            alert("ERROR");
-        }
-    }
-    else {
-        t = setTimeout(autoLeaveGoogleMeet, 5000); //here it ll call itself after 5 seconds
-    }
-}
+
 
 function checkBeforeStart() {
-    countValue = document.getElementById('countValue').value;
-    alert(`${countValue}`)
-    if (document.querySelector(".gV3Svc>span").nextElementSibling.innerText > countValue) {
+
+    function autoLeaveGoogleMeet() {
+
+        alert("checking members.....");
+        //check for active members in meet
+        if (document.querySelector(".gV3Svc>span").nextElementSibling.innerText < count) {
+            try {
+                window.document.querySelector(".FbBiwc").click(); //click the endcall btn on meet
+                alert('Ooo...Meet has been ended');
+            } catch {
+                alert("ERROR2");
+            }
+        }
+        else {
+            t = setTimeout(autoLeaveGoogleMeet, 5000); //here it ll call itself after 5 seconds
+        }
+    }
+
+    count = prompt("Enter count");
+    alert(`${count}`)
+    if (document.querySelector(".gV3Svc>span").nextElementSibling.innerText > count) {
 
         try {
-            alert(`more than ${x} are here!`);
+            alert(`more than ${count} people are here!`);
             autoLeaveGoogleMeet()
         } catch {
-            alert("ERROR");
+            alert("ERROR1");
         }
     } else {
         alert(`Check not yet started`);
@@ -41,7 +45,7 @@ function checkBeforeStart() {
 
 
 
-// When the button is clicked, inject setPageBackgroundColor into current page
+// When the button is clicked, inject function will execute on current page
 startButton.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
