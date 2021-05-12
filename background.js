21cb656aa534d34d7965e46e2ae0867f7856f694
harshithva <harshith11032001@gmail.com>
2021-05-12 23:00:17 +0530

    msg feature added

--- background.js
+++ background.js
@@ -1,6 +1,24 @@
-let color = '#3aa757';
+
+
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
+    // chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
+
 });
+
+
+
+// chrome.notifications.create(options);
+
+chrome.runtime.onMessage.addListener(data => {
+    if (data.type === 'notification') {
+        // data.chrome.notifications.create('', data.options);
+        // chrome.alarms.create({ delayInMinutes: 5 })
+    }
+
+    console.log(data);
+});
+
+
+// chrome.notifications.create(options);
--- popup.js
+++ popup.js
@@ -1,42 +1,61 @@
 
-
+// let count = document.getElementById('count').value;
 let t = null;
 let startButton = document.getElementById('startButton');
-let count = 40;
+
 
 function start() {
+    let count = prompt("Enter the exiting count")
+    function update(msg) {
+        let ele = document.getElementById('meet_bot_message')
+        if (ele) {
+            ele.remove();
+        }
 
-    count = prompt("Enter exiting count")
+        theParent = document.querySelector('.f0WtFf');
+        theKid = document.createElement("div");
+        theKid.innerHTML = `<div id="meet_bot_message" jscontroller="U1Cub" jsaction="JIbuQc:QODBq;rcuQ6b:Q313Se" role="switch" class="Q8K3Le" aria-checked="false">
+        <div jsshadow="" role="button" class="uArJ5e UQuaGc kCyAyd QU4Gid SyIese M9Bg4d" jscontroller="VXdfxd" jsaction="click:cOuCgd; mousedown:UX7yZ; mouseup:lbsD7e; mouseenter:tfO1Yc; mouseleave:JywGue;touchstart:p6p2H; touchmove:FwuNnf; touchend:yfqBxc(preventMouseEvents=true|preventDefault=true); touchcancel:JMtRjd;focus:AHmuwe; blur:O22p3e; contextmenu:mg9Pef" jsname="r8qRAd" aria-label="Controlled by bot" tabindex="0" data-tooltip="Controlled by bot" data-tooltip-vertical-offset="-12" data-tooltip-horizontal-offset="0"><div class="Fvio9d MbhUzd" jsname="ksKsZd"></div><div class="e19J0b CeoRYc"></div><span jsslot="" class="l4V7wb Fxmcue"><span class="NPEfkd RveJvd snByac"><div class="n8i9t"><div class="XFtqNb"><i class="google-material-icons" aria-hidden="true">smart_toy</i></div><div class="I98jWb">
+        ${msg}</div>
+        </div>
+        </span>
+        </span>
+        </div>
+        </div>`
+        theParent.insertBefore(theKid, theParent.firstChild);
+    }
+    update(`Waiting to reach more than ${count}`)
 
     function autoLeaveGoogleMeet() {
 
-        // alert("checking members.....");
         //check for active members in meet
+        update(`Checking for active members - ${document.querySelector(".gV3Svc>span").nextElementSibling.innerText}`)
         if (document.querySelector(".gV3Svc>span").nextElementSibling.innerText < count) {
             try {
                 window.document.querySelector(".FbBiwc").click(); //click the endcall btn on meet
-                // alert('Ooo...Meet has been ended');
+                // update(`You have exited successfully`)
             } catch {
                 alert("ERROR2");
             }
         }
         else {
+            update(`I will end call once active members is less than ${count}`)
             t = setTimeout(autoLeaveGoogleMeet, 5000); //here it ll call itself after 5 seconds
         }
     }
 
 
     function checkBeforeStart() {
+
         if (document.querySelector(".gV3Svc>span").nextElementSibling.innerText > count) {
 
             try {
-                // alert(`more than ${count} people are here!`);
+                update(`More than ${count} people are here!`)
                 autoLeaveGoogleMeet()
             } catch {
                 alert("ERROR1");
             }
         } else {
-            // alert(`Check not yet started`);
             t = setTimeout(checkBeforeStart, 5000);
         }
     }
@@ -52,10 +71,9 @@ function start() {
 
 
 
-
-
 // When the button is clicked, inject function will execute on current page
 startButton.addEventListener("click", async () => {
+
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
     chrome.scripting.executeScript({
@@ -63,4 +81,3 @@ startButton.addEventListener("click", async () => {
         function: start,
     });
 });
-
