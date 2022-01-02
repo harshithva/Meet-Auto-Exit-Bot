d258587f9151e266ecbc03716c4ecf77f142af69
harshithva <harshith11032001@gmail.com>
2022-01-02 09:40:11 +0530

    error fixed

--- background.js
+++ background.js
@@ -3,7 +3,7 @@
 
 chrome.runtime.onInstalled.addListener(() => {
     // chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
+    console.log('Default background color set to');
 
 });
 
